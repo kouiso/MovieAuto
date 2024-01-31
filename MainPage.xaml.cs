bba44b31a336ae7475705c9e375c5d1a75aa5819
KosukeIsogai <kouiso@ritmo.co.jp>
2024-01-31 16:44:30 +0900

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieAuto
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MovieAuto
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
